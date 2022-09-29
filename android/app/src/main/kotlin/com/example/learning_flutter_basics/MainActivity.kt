1800ea99a2529128b5dc9d50e115dfe3bcd0ff14
Raghav Maheshwari <raghav.maheshwari@nagarro.com>
2022-09-29 17:04:10 +0530

    Final_Touch

---
+++ android/app/src/main/kotlin/com/example/learning_flutter_basics/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learning_flutter_basics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/result.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final VoidCallback restHandler;
+
+  Result(this.resultScore, this.restHandler);
+
+  String get resultPhrase{
+    var resultText = 'You did it!';
+    if(resultScore <= 8){
+      resultText = 'You are awesome and innocent!';
+    }
+    else if(resultScore <= 12){
+      resultText = 'Pretty likeable!';
+    }
+    else if(resultScore < 16){
+      resultText = 'You are ... strange?';
+    }
+    else{
+      resultText = 'You are so bad!';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            onPressed: restHandler,
+            child: const Text('Restart Quiz'),
+          )
+        ],
+      ),
+    );
+  }
+}
